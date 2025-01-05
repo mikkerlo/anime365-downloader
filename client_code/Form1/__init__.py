de7202cb102ad06f1e4809cde925fb19069eb8e2
Anton Kvasha <34814927+akvasha@users.noreply.github.com>
2025-01-05 05:36:40 +0300

    Better logic

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -186,7 +186,7 @@ class Form1(Form1Template):
       tr_id = ep_ui.download_ep.tr_id
       # ep_ui.ep_info.episodeInt is string, but who cares?
       episode_number = float(ep_ui.ep_info.episodeInt)
-      fractional_part = round(episode_number % 1)
+      fractional_part = round(episode_number % 1, 1)
       if fractional_part:
           formatted_number = f"{int(episode_number):02d}.{fractional_part}"
       else:
