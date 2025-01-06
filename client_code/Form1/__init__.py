e1e9d690dd4f6ae6c50590b43968e4ccded9d16c
mikhail.semenov.al@gmail.com <mikhail.semenov.al@gmail.com>
2025-01-06 12:21:57 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -184,11 +184,11 @@ class Form1(Form1Template):
       if not ep_ui.enabled_button.checked:
         continue
       tr_id = ep_ui.download_ep.tr_id
-      # ep_ui.ep_info.episodeInt is string, but who cares?
-      episode_number = float(ep_ui.ep_info.episodeInt)
-      fractional_part = round(episode_number % 1, 1)
-      if fractional_part:
-          formatted_number = f"{int(episode_number):02d}.{fractional_part}"
+      episode_number = ep_ui.ep_info.episodeInt
+      if episode_number % 1 == 0.5:
+          formatted_number = f"{int(episode_number):02d}.5"
+      else:
+          formatted_number = f"{int(episode_number):02d}"
       name = f"{self.anime_info.anime_name} - {formatted_number}"
       url = anvil.server.call('get_direct_link', tr_id, name, access_token)
       print('url:', url)
